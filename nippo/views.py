8f151559ae7dc2e8cdeae6ff638ee9024dd3b13c
haruto-terashima <hal.xyz@icloud.com>
2025-11-21 03:31:50 +0900

    add nippo app

---
+++ nippo/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import NippoModel
+from .forms import NippoFormClass
+
+def nippoListView(request):
+    template_name = "nippo/nippo-list.html"
+    ctx = {}
+    qs = NippoModel.objects.all()
+    ctx["object_list"] = qs
+    return render(request, template_name, ctx)
+
+
+def nippoDetailView(request, pk):
+    template_name="nippo/nippo-detail.html"
+    ctx = {}
+    #q = NippoModel.objects.get(pk=pk)
+    q = get_object_or_404(NippoModel, pk=pk)
+    ctx["object"] = q
+    return render(request, template_name, ctx)
+
+def nippoCreateView(request):
+    template_name = "nippo/nippo-form.html"
+    form=NippoFormClass(request.POST or None)
+    ctx = {}
+    ctx["form"] = form
+    if form. is_valid():
+        title = form.cleaned_data["title"]
+        content = form.cleaned_data["content"]
+        obj = NippoModel(title = title, content = content)
+        obj.save()
+        return redirect("nippo-list")
+    return render(request, template_name, ctx)
+
+def nippoUpdateFormView(request,pk):
+    template_name = "nippo/nippo-form.html"
+    #obj = NippoModel.objects.get(pk=pk)
+    q = get_object_or_404(NippoModel, pk=pk)
+    initial_values = {"title":obj.title, "content":obj.content}
+    form = NippoFormClass(request.POST or initial_values)
+    ctx={"form":form}
+    ctx["object"] = obj
+    if form.is_valid():
+        title = form.cleaned_data["title"]
+        content = form.cleaned_data["content"]
+        obj.title = title
+        obj.content = content
+        obj.save()
+        if request.POST:
+            return redirect("nippo-list")
+    return render(request, template_name, ctx)
+
+def nippoDeleteView(request, pk):
+    template_name = "nippo/nippo-delete.html"
+    obj = get_object_or_404(NippoModel, pk=pk)
+    ctx = {"object": obj}
+    if request.POST:
+        obj.delete()
+        return redirect("nippo-list")
+    return render(request, template_name, ctx)
+
